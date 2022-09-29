531aba8d5ae16b30370ab86ad04423e2d03e07b6
Mokom Geh <mokomgeh45@gmail.com>
2022-09-29 17:11:29 -0400

    installed  & setup debug_toolbar

--- config/urls.py
+++ config/urls.py
@@ -1,15 +1,17 @@
+import debug_toolbar
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('account/', include("account.urls", namespace="account")),
-    path('cart/', include("cart.urls", namespace="cart")),
-    path('payment/', include("payment.urls", namespace="payment")),
-    path('orders/', include("orders.urls", namespace="orders")),
-    path('', include("store.urls", namespace="store")),
+    path("admin/", admin.site.urls),
+    path("account/", include("account.urls", namespace="account")),
+    path("cart/", include("cart.urls", namespace="cart")),
+    path("payment/", include("payment.urls", namespace="payment")),
+    path("orders/", include("orders.urls", namespace="orders")),
+    path("", include("store.urls", namespace="store")),
+    path("__debug__/", include(debug_toolbar.urls)),
 ]
 
 if settings.DEBUG:
